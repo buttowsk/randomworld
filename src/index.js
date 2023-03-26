b205ba79bdb0fb74665e07c5d6c9fd70ec487dc9
buttowsk <vinicius_nunes0104@hotmail.com>
2023-03-26 02:48:53 -0300

    hashRouter pro gh pages

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {GlobalStyle} from "./globalStyles/styles"
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <GlobalStyle />
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
